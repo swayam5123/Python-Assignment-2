46ab1b9970c3475876d75a4777ee0055a97d0c8f
swayam5123 <swayamshelar12@gmail.com>
2025-07-12 14:42:41 +0530

    Add files via upload

---
+++ Assignment 2 Task 1.py
@@ -0,0 +1,7 @@
+a=int(input("Enter a number: "))
+
+if a%2==0:
+    print("Given Number is even")
+else:
+    print("Given Number is odd")
+print("Thank you")
---
+++ Assignment 2 Task 2.py
@@ -0,0 +1,4 @@
+x=0
+for i in range(1,51):
+    x=x+i
+print("the sum of numbers from 1 to 5 is ",x)
